b498f1302c85bcc7665350d911962d5e11ea0d96
Javiera <69701833+javiesp@users.noreply.github.com>
2023-11-07 20:47:24 -0300

    component login-psa
    Se añade componente login-psa junto con su front end

---
+++ psa_templates/src/app/login-psa/login-psa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-psa',
+  templateUrl: './login-psa.component.html',
+  styleUrls: ['./login-psa.component.css']
+})
+export class LoginPsaComponent {
+
+}
