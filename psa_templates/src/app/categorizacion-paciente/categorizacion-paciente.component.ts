03f9c8a78df58c9903db027224e3996e3dad45d7
Javiera <69701833+javiesp@users.noreply.github.com>
2023-11-09 21:07:09 -0300

    desarrollo inicial lógica cuestionario

--- psa_templates/src/app/categorizacion-paciente/categorizacion-paciente.component.ts
+++ psa_templates/src/app/categorizacion-paciente/categorizacion-paciente.component.ts
@@ -1,45 +1,65 @@
-import { Component, ViewEncapsulation  } from '@angular/core';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-categorizacion-paciente',
   templateUrl: './categorizacion-paciente.component.html',
-  styleUrls: ['./categorizacion-paciente.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./categorizacion-paciente.component.css']
 })
 export class CategorizacionPacienteComponent {
-  opcionSeleccionada: string = '';
-  opcion1: boolean = false; // Variable para la opción 1
-  opcion2: boolean = false; // Variable para la opción 2
-  opcion3: boolean = false; // Variable para la opción 3
-  task: any = {
-    name: 'Indeterminate',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      { name: 'Primary', completed: false, color: 'primary' },
-      { name: 'Accent', completed: false, color: 'accent' },
-      { name: 'Warn', completed: false, color: 'warn' },
-    ],
+  respuestas: { [key: string]: boolean | number } = {
+    'Experimenta dolor': false,
+    'Signo de hemorragia': false,
+    'Cambios en la conciencia': false,
+    'Fiebre o temperatura anormal': false,
+    'Tiempo de los síntomas': 0 // Inicializado con menos de 6 horas
+    // Agrega el resto de preguntas
   };
-  
-  opcionesSeleccionadas() {
-    if (this.opcion1) {
-      console.log('Opción 1 seleccionada');
-      // Realizar acciones específicas para la opción 1
+
+  nivel_prioridad: number = 5; // Inicializado en 5 por defecto
+
+  evaluarPrioridad() {
+    if (this.respuestas['Experimenta dolor']) {
+      const nivel_dolor = parseInt(prompt("En una escala del 1 al 10, ¿cómo describiría el dolor?", '') || '0');
+      if (nivel_dolor >= 7) {
+        this.nivel_prioridad = 1;
+      } else if (nivel_dolor >= 4) {
+        this.nivel_prioridad = 2;
+      } else {
+        this.nivel_prioridad = 3;
+      }
     }
 
-    if (this.opcion2) {
-      console.log('Opción 2 seleccionada');
-      // Realizar acciones específicas para la opción 2
+    if (this.respuestas['Signo de hemorragia']) {
+      this.nivel_prioridad = 1;
     }
 
-    if (this.opcion3) {
-      console.log('Opción 3 seleccionada');
-      // Realizar acciones específicas para la opción 3
+    if (this.respuestas['Cambios en la conciencia']) {
+      this.nivel_prioridad = 2;
     }
+
+    if (this.respuestas['Fiebre o temperatura anormal']) {
+      this.nivel_prioridad = 3;
+    }
+
+    const tiempo_sintomas = this.respuestas['Tiempo de los síntomas'] as number; // Asegura que sea tratado como un número
+    if (tiempo_sintomas <= 6) {
+      this.nivel_prioridad = 3;
+    } else if (tiempo_sintomas > 6 && tiempo_sintomas <= 12) {
+      this.nivel_prioridad = 4;
+    } else {
+      this.nivel_prioridad = 5;
+    }
+
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivel_prioridad}`);
   }
 
+  responderPregunta(pregunta: string, respuesta: boolean) {
+    this.respuestas[pregunta] = respuesta;
+    this.evaluarPrioridad();
+  }
+
+  cambiarTiempoSintomas(valor: number) {
+    this.respuestas['Tiempo de los síntomas'] = valor;
+    this.evaluarPrioridad();
+  }
 }
