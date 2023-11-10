37aff7c0e057a3b8aff335627fbc26a0dfb45644
Javiera <69701833+javiesp@users.noreply.github.com>
2023-11-10 19:35:07 -0300

    lógica cuestioanario bug
    pequeño bug a arreglar con la lógica del cuestionario de categorización

--- psa_templates/src/app/app.module.ts
+++ psa_templates/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- psa_templates/src/app/categorizacion-paciente/categorizacion-paciente.component.ts
+++ psa_templates/src/app/categorizacion-paciente/categorizacion-paciente.component.ts
@@ -3,63 +3,118 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-categorizacion-paciente',
   templateUrl: './categorizacion-paciente.component.html',
-  styleUrls: ['./categorizacion-paciente.component.css']
+  styleUrls: ['./categorizacion-paciente.component.css'],
+  
 })
 export class CategorizacionPacienteComponent {
   respuestas: { [key: string]: boolean | number } = {
     'Experimenta dolor': false,
     'Signo de hemorragia': false,
     'Cambios en la conciencia': false,
-    'Fiebre o temperatura anormal': false,
-    'Tiempo de los síntomas': 0 // Inicializado con menos de 6 horas
-    // Agrega el resto de preguntas
+    'comportamiento anormal': false,
+    'situacion catastrofe': false,
+    'Tiempo de los síntomas': 0
   };
 
-  nivel_prioridad: number = 5; // Inicializado en 5 por defecto
+  nivelPrioridad: number = 5; 
+  categoriaPaciente: string = '';
 
-  evaluarPrioridad() {
-    if (this.respuestas['Experimenta dolor']) {
-      const nivel_dolor = parseInt(prompt("En una escala del 1 al 10, ¿cómo describiría el dolor?", '') || '0');
-      if (nivel_dolor >= 7) {
-        this.nivel_prioridad = 1;
-      } else if (nivel_dolor >= 4) {
-        this.nivel_prioridad = 2;
+  
+
+  evaluaDolorPaciente() {
+    if (this.respuestas['Experimenta dolor'] == true) {
+      const nivelDolor = parseInt(prompt("En una escala del 1 al 10, ¿cómo describiría el dolor?", '') || '0');
+      if (nivelDolor >= 7) {
+        this.nivelPrioridad = 1;
+      } else if (nivelDolor >= 4) {
+        this.nivelPrioridad = 2;
       } else {
-        this.nivel_prioridad = 3;
+        this.nivelPrioridad = 3;
       }
-    }
-
-    if (this.respuestas['Signo de hemorragia']) {
-      this.nivel_prioridad = 1;
-    }
-
-    if (this.respuestas['Cambios en la conciencia']) {
-      this.nivel_prioridad = 2;
-    }
-
-    if (this.respuestas['Fiebre o temperatura anormal']) {
-      this.nivel_prioridad = 3;
-    }
-
-    const tiempo_sintomas = this.respuestas['Tiempo de los síntomas'] as number; // Asegura que sea tratado como un número
-    if (tiempo_sintomas <= 6) {
-      this.nivel_prioridad = 3;
-    } else if (tiempo_sintomas > 6 && tiempo_sintomas <= 12) {
-      this.nivel_prioridad = 4;
     } else {
-      this.nivel_prioridad = 5;
+      this.nivelPrioridad = 5;
     }
-
-    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivel_prioridad}`);
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
   }
 
-  responderPregunta(pregunta: string, respuesta: boolean) {
+  evaluaSignoHemorragia() { // Pregunta 1
+    if (this.respuestas['Signo de hemorragia'] == true) {
+      this.nivelPrioridad = 1;
+    } else {
+      this.nivelPrioridad = this.nivelPrioridad
+    }
+
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
+  }
+
+  evaluaCambioConsiencia() { // Pregunta 2
+    if (this.respuestas['Cambios en la conciencia'] == true) {
+      this.nivelPrioridad = 2;
+    } else {
+      this.nivelPrioridad  = this.nivelPrioridad
+    }
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
+  }
+
+  evaluaComportamientoAnormal() { // Pregunta 3 
+    if (this.respuestas['comportamiento anormal'] == true) {
+      this.nivelPrioridad = 3;
+    } else {
+      this.nivelPrioridad  = this.nivelPrioridad
+    }
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
+  }
+ 
+  evaluaSituacionCatastrofe() { // Pregunta 4
+    if (this.respuestas['situacion catastrofe'] == true) {
+      this.nivelPrioridad = 3;
+    } else {
+      this.nivelPrioridad  = this.nivelPrioridad
+    }
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
+  }
+
+  tiempoSintomasPaciente() { // Funcion que evalua el tiempo de los sintomas y asigana puntaje 
+    const tiempoSintomas = this.respuestas['Tiempo de los síntomas'] as number; // interpreta como number
+    if (tiempoSintomas <= 6) {
+      this.nivelPrioridad = this.nivelPrioridad;
+    } else if (tiempoSintomas > 6 && tiempoSintomas <= 12) {
+      this.nivelPrioridad = 3;
+    } else {
+      this.nivelPrioridad = 1;
+    }
+    
+    console.log(`El paciente ha sido categorizado como nivel de prioridad: ${this.nivelPrioridad}`);
+  }
+
+  responderPregunta1(pregunta: string, respuesta: boolean) {
     this.respuestas[pregunta] = respuesta;
-    this.evaluarPrioridad();
+    this.evaluaDolorPaciente();
+  }
+
+  responderPregunta2(pregunta: string, respuesta: boolean) {
+    this.respuestas[pregunta] = respuesta;
+    this.evaluaSignoHemorragia();
+  }
+
+  responderPregunta3(pregunta: string, respuesta: boolean) {
+    this.respuestas[pregunta] = respuesta;
+    this.evaluaCambioConsiencia();
   }
 
   cambiarTiempoSintomas(valor: number) {
     this.respuestas['Tiempo de los síntomas'] = valor;
-    this.evaluarPrioridad();
+    this.tiempoSintomasPaciente();
+  }
+
+  categorizacionPaciente() { // Categoriza al paciente segun su nivel de prioridad
+    console.log(`PRIORIDAD PACIENTE: ${this.nivelPrioridad}`);
+    if (this.nivelPrioridad = 1){
+      this.categoriaPaciente = 'C1';
+      const nivelDolor = prompt("UD debe ir a meson. Su nivel es:", this.categoriaPaciente);
+    } else if (this.nivelPrioridad = 5) {
+      this.categoriaPaciente = 'C4';
+      const nivelDolor = prompt("Continue con la evaluacion. Su nivel es:", this.categoriaPaciente);
+    }
   }
 }
