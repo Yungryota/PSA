0ebb91279fdba7b537e1c2a3dd11041e2b559b69
Javiera <69701833+javiesp@users.noreply.github.com>
2023-11-26 22:47:33 -0300

    aaaaaaaaaaaaaaa
    sufro, esta vida qla no me merece, adios vida, solo quiero desaparecer

--- psa_templates/src/app/vistas/cuestionario-consulta/cuestionario-consulta.component.ts
+++ psa_templates/src/app/vistas/cuestionario-consulta/cuestionario-consulta.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NeuralNetwork } from 'brain.js';
+
 
 @Component({
   selector: 'app-cuestionario-consulta',
@@ -8,8 +10,8 @@ import { Component } from '@angular/core';
 export class CuestionarioConsultaComponent {
    respuestaPregunta1: string = '';
    respuesta: Array<string> = [];
-
-
+   net = new NeuralNetwork();
+   prompIa: string = '';
   
    guardarPreguntas(resp: string){
     this.respuesta.push(resp); 
@@ -18,9 +20,42 @@ export class CuestionarioConsultaComponent {
   }
   
   guardarEdad(edad: string){
-    this.respuesta.push('Edad: ' + edad); // Agrega la edad al arreglo de respuestas
+    this.respuesta.push('Edad: ' + edad); 
     console.log('Edad ingresada:', edad);
     console.log(this.respuesta);
   }
+
+  trainingData: { input: any, output: any }[] = [
+    {
+      input: "genera un diagnostico en 5 palabras para estos sintomas: Edad: 2, Sexo: Femenino, ConsumeTabaco: no, ConsumeAlcohol: no, Tos y dolor en el pecho, dificultad respiratoria, Fiebre: si, Dificultad al dormir, Diabetes: no, No padece enfermedad arterial, Enfermedad pulmonar: no, Hipertension: no, Sin dolor",
+      output: "Ejemplo de diagnóstico para este conjunto de síntomas"
+    },
+  ];
+  
+
+  train(trainingData: { input: any, output: any }[]): void {
+    this.net.train(trainingData);
+  }  
+
+  askQuestion() {
+    const frasePrompt = "Genera un diagnóstico en 5 palabras para estos síntomas:";
+    const sintomasUsuario = this.respuesta; 
+    const newQuestion = `${frasePrompt} ${sintomasUsuario}`;
+    
+    this.prompIa = this.predictAnswer(newQuestion);
+
+    console.log(this.prompIa);
+  }
+  
+  predictAnswer(newQuestion: any): any {
+    const prediction = this.net.run(newQuestion);
+    return prediction;
+
+  }
+  
+  
+
+  
+
 
 }
