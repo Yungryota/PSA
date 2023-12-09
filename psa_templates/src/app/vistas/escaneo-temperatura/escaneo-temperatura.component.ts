a71da489a1d3454284a8e1d8626b99f9c18cc9b8
Svboibg20 <hu.gonzalez@duocuc.cl>
2023-12-08 23:45:54 -0300

    AHORA SI LA IMAGEN EN SWEET
    :)

--- psa_templates/src/app/vistas/escaneo-temperatura/escaneo-temperatura.component.ts
+++ psa_templates/src/app/vistas/escaneo-temperatura/escaneo-temperatura.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { ServiceService } from 'src/app/Service/service.service';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -15,7 +15,14 @@ export class EscaneoTemperaturaComponent implements OnInit{
 
   constructor(private service: ServiceService, private router: Router){}
   ngOnInit(): void {
-    
+    Swal.fire({
+      icon: "info",
+      title: "Ejemplo de toma de temperatura.",
+      imageUrl: "https://media.discordapp.net/attachments/1139632235218223154/1182796270176309288/image.png?ex=6585ffde&is=65738ade&hm=6e02e240c7582e2f3544f2d5f02f0f3b91eee0aedff9482c156fbf427e66ecbf&=&format=webp&quality=lossless&width=1089&height=662",
+      imageWidth: 500,
+      imageHeight: 300,
+      imageAlt: "Custom image"
+    });
     console.log('aca el modal con la foto');
   }
 
@@ -34,6 +41,8 @@ export class EscaneoTemperaturaComponent implements OnInit{
       (error: any) => {
         console.error(error); 
       }
+
+      
     );
   }
 
